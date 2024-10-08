217144beffc84fe9b6c9875f626b7aa0ad1d3716
Sisira-pottabathini <sisira3605@gmail.com>
2024-10-09 00:24:03 +0530

    first commit

---
+++ sample13/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return(
+  <div>
+    <h2 style={{color:'blue'}}>welcome to jsx code</h2>
+    <h3>Welcome to JSX code</h3>
+  </div>
+  )
+}
+export default App
